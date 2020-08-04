f69d43b75db8e97f351cb2df57d1de4b5aa03819
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-08-04 17:34:59 +0900

    refactor: createConnection to be return

--- src/service/TaskService.ts
+++ src/service/TaskService.ts
@@ -1,21 +1,30 @@
 import { Request, Response } from "express";
 import {Task} from "../entity/Task";
-import {getRepository} from "typeorm";
+import {createConnection, getRepository} from "typeorm";
 
 export class TaskService {
 
     public getTasks = async(): Promise<Array<Task>> => {
-        return await getRepository(Task).find();
+        let tasks: Array<Task>;
+        return createConnection().then(async() => {
+            tasks = await getRepository(Task).find();
+            return tasks;
+        })
     }
 
     public writeTask = async(task: Task): Promise<void> => {
-        const list: Task[] = await getRepository(Task).find();
-        list.push(task);
-        await getRepository(Task).save(list);
+        let tasks: Task[];
+        createConnection().then(async() => {
+            tasks = await getRepository(Task).find();
+            tasks.push(task);
+            await getRepository(Task).save(tasks);
+        })
     }
 
     public deleteTask = async(id: number): Promise<void> => {
-        const task: Task = await getRepository(Task).findOne(id);
-        await getRepository(Task).delete(task);
+        createConnection().then(async() => {
+            const task: Task = await getRepository(Task).findOne(id);
+            await getRepository(Task).delete(task);
+        })
     }
 }
